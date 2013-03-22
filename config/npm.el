f755909de8de5aaa787647885f3d8fa6277a67e9
E.Azer Koçulu <azer@kodfabrik.com>
2013-03-22 15:57:03 -0700

    better markdown

--- config/npm.el
+++ config/npm.el
@@ -1,4 +1,8 @@
 (add-lib-path "npm")
 (require 'npm)
 
-(global-set-key (kbd "M-n") 'npm-new)
+(global-set-key (kbd "M-n i") 'npm-install)
+(global-set-key (kbd "M-n n") 'npm-new)
+(global-set-key (kbd "M-n d") 'npm-new-dependency)
+(global-set-key (kbd "M-n p") 'npm-publish)
+(global-set-key (kbd "M-n v") 'npm-version)
