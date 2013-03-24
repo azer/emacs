e0e5bf89b8441e262ef1834b41075341b93da931
E.Azer Koçulu <azer@kodfabrik.com>
2013-03-23 23:24:32 -0700

    new npm bindings

--- config/npm.el
+++ config/npm.el
@@ -4,5 +4,7 @@
 (global-set-key (kbd "M-n i") 'npm-install)
 (global-set-key (kbd "M-n n") 'npm-new)
 (global-set-key (kbd "M-n d") 'npm-new-dependency)
+(global-set-key (kbd "M-n e") 'npm-nodemon-exec)
 (global-set-key (kbd "M-n p") 'npm-publish)
+(global-set-key (kbd "M-n t") 'npm-test)
 (global-set-key (kbd "M-n v") 'npm-version)
