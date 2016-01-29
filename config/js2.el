0146712c28a6f0ab8006ba0e784b3367b8e5e4bb
E.Azer Koçulu <azer@kodfabrik.com>
2016-01-29 06:43:34 -0800

    ignore missing semicolons in js2-mode

--- config/js2.el
+++ config/js2.el
@@ -6,6 +6,7 @@
 (add-to-list 'auto-mode-alist '("\\.json$" . js2-mode))
 
 (custom-set-variables '(js2-strict-inconsistent-return-warning nil))
+(custom-set-variables '(js2-strict-missing-semi-warning nil))
 
 (setq js-indent-level 2)
 (setq js2-indent-level 2)
