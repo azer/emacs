ebe0497494cfffc4c52c45ba155d31dd0ad4fe70
E.Azer Koçulu <azer@kodfabrik.com>
2016-02-13 13:08:00 -0800

    add set-go-path

--- config/golang.el
+++ config/golang.el
@@ -26,3 +26,22 @@
 
 (add-hook 'go-mode-hook (lambda ()
                           (local-set-key (kbd "C-c d") 'godoc-at-point)))
+
+(defun auto-complete-for-go ()
+  (auto-complete-mode 1))
+
+(add-hook 'go-mode-hook 'auto-complete-for-go)
+
+(defun set-go-path ()
+  "Set GOPATH and GOBIN"
+  (interactive)
+  (let (gopath)
+    (setq gopath (read-from-minibuffer "GOPATH: " (file-name-directory (or load-file-name buffer-file-name))))
+    (message (concat "Setting GOPATH to " gopath))
+    (setenv "GOPATH" gopath)
+    (setenv "GOBIN" (concat gopath "/bin"))
+    )
+  )
+
+(add-hook 'go-mode-hook (lambda ()
+                          (local-set-key (kbd "C-c p") 'set-go-path)))
